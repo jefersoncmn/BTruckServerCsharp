d9756589e938dfcd0c9deff2c903813eb812a9b0
jefersoncmn <jefersoncmn@hotmail.com>
2022-02-05 13:40:43 -0300

    first commit

---
+++ GameServer/ServerSend.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class ServerSend
+    {
+        private static void SendTCPData(int _toClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            Server.clients[_toClient].tcp.SendData(_packet);
+        }
+
+        /// <summary>
+        /// Send TCP data to all clients
+        /// </summary>
+        /// <param name="_packet"></param>
+        private static void SendTCPDataToAll(Packet _packet)
+        {
+            _packet.WriteLength();
+            for(int i = 1; i <= Server.MaxPlayers; i++)
+            {
+                Server.clients[i].tcp.SendData(_packet);
+            }
+        }
+
+        /// <summary>
+        /// Send TCP data to all client except one
+        /// </summary>
+        /// <param name="_exceptClient"></param>
+        /// <param name="_packet"></param>
+        private static void SendTCPDataToAll(int _exceptClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= Server.MaxPlayers; i++)
+            {
+                if(i != _exceptClient)
+                {
+                    Server.clients[i].tcp.SendData(_packet);
+                }
+                
+            }
+        }
+
+        public static void Welcome(int _toClient, string _msg)
+        {
+            using(Packet _packet = new Packet((int)ServerPackets.welcome))
+            {
+                _packet.Write(_msg);
+                _packet.Write(_toClient);
+
+                SendTCPData(_toClient, _packet);
+            }
+        }
+    }
+}
